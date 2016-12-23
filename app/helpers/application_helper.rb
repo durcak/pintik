1ebf2769dca26ec00d25fd4921ece0f70342ded3
durcak <durcaktomas@gmail.com>
2016-12-23 15:30:25 +0100

    news feed views added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def relative_time(created_at)
+    time_ago_in_words(created_at)
+  end
+
+  def formatted_time(time)
+    time.to_formatted_s(:short)
+  end
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,6 @@
 class Follow < ActiveRecord::Base
 	include PublicActivity::Model
-	tracked only: [:create, :destroy]#, owner: Proc.new{ |controller, model| model.user }
+	tracked only: [:create] #owner: Proc.new{ |controller, model| controller.current_user }
 
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
